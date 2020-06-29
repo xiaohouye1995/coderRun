601b4fc93e041492ad9c863fb98da4fcf18a3490
zhouxiaoyu1994 <363993481@qq.com>
2020-06-29 17:15:52 +0800

    add

--- polished_project/polished_project/temp/quick-scripts/src/__qc_index__.js
+++
@@ -1,7 +0,0 @@
-
-require('./assets/migration/use_v2.0.x_cc.Toggle_event');
-require('./assets/scripts/Game');
-require('./assets/scripts/Player');
-require('./assets/scripts/ScoreAnim');
-require('./assets/scripts/ScoreFX');
-require('./assets/scripts/Star');
